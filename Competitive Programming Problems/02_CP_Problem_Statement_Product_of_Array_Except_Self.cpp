751691ead00ed38fae864e1a2fd75f5cc4358692
devendra dhare <devendradhare03@gmail.com>
2022-10-11 23:56:01 +0530

    Competitive Programming Problem 1 and 2 is complete

---
+++ Competitive Programming Problems/02_CP_Problem_Statement_Product_of_Array_Except_Self.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[4] = {1, 2, 3, 4}, ans[4];
+    int j = 0, multi = 1;
+    for (int i = 0; i < 4; i++)
+    {
+        if (i != j)
+        {
+            multi *= arr[i];
+        }
+        if (i == 3)
+        {
+            ans[j] = multi;
+            multi = 1;
+            i = 0;
+            j++;
+        }
+        if (j == 3)
+        {
+            break;
+        }
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        cout << ans[i]<<" ";
+    }
+    return 0;
+}
